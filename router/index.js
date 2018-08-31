4e370f80755e153e9672be3161e9b0a02ccb255c
zcong1993 <1005997919@qq.com>
2018-08-31 17:04:43 +0800

    feat(github): add github oauth

--- router/index.js
+++ router/index.js
@@ -21,46 +21,54 @@ router.get('/logout', ctx => {
   ctx.redirect('/')
 })
 
-router.get('/auth/btcc', ctx => {
+const state = 'xsbnhjcbdjhbcjdxs'
+
+router.get('/auth/github', ctx => {
   const params = {
     client_id: process.env.CLIENT_ID, // your client_id
     redirect_uri: 'http://localhost:3000/auth/callback',
-    response_type: 'code',
-    scope: 'place-orders user-information need-jwt' //  scope grant
+    state
   }
-  const redirectUrl = `${process.env.BASE_URL}/oauth/authorize?${qs.stringify(params)}`
+  const redirectUrl = `https://github.com/login/oauth/authorize?${qs.stringify(params)}`
   return ctx.redirect(redirectUrl)
 })
 
 router.get('/auth/callback', async ctx => {
-  const { code } = ctx.request.query
+  const {
+    code
+  } = ctx.request.query
   if (!code) {
     return ctx.redirect('/')
   }
-  try{
-      // using code get access_token, refresh_token and expires_in
-    const resp = await axios.post(`${process.env.BASE_URL}/oauth/token`, {
-      grant_type: 'authorization_code',
+
+  try {
+    const params = {
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
       redirect_uri: 'http://localhost:3000/auth/callback',
-      code
-    })
-    // now you get token_type, access_token, refresh_token and expires_in
-    const { token_type, access_token } = resp.data
-    // using access_token get user profile
-    const profileResp = await axios({
-      url: `${process.env.BASE_URL}/api/user`,
+      code,
+      state
+    }
+
+    const resp = await axios.post('https://github.com/login/oauth/access_token', params)
+    const {
+      access_token
+    } = qs.parse(resp.data)
+
+    const userResp = await axios({
+      url: 'https://api.github.com/user',
       method: 'GET',
       headers: {
-        Authorization: `${token_type} ${access_token}`
+        Authorization: `token ${access_token}`
       }
     })
+
     // now you get user profile
-    ctx.session.user = profileResp.data
+    ctx.session.user = userResp.data
     // do your own login logic here
     return ctx.redirect('/secret')
   } catch (err) {
+    console.log(err)
     return ctx.redirect('/')
   }
 })
